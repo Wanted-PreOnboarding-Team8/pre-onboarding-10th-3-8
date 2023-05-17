e549db133198f0fda788da09194f026f73fcfa57
kimyonghui <kyhui1115@gmail.com>
2023-05-17 16:53:14 +0900

    Feat: useTodoHook 기능 추가
    Co-authored-by: powercording <powercording@users.noreply.github.com>
    Co-authored-by: 임예지 <1myeji@users.noreply.github.com>
    Co-authored-by: shaqok <shaqok@users.noreply.github.com>

---
+++ src/hooks/useTodoHook.ts
@@ -0,0 +1,49 @@
+import React, { useState, useCallback, useEffect, type FC } from 'react';
+import { createTodo, deleteTodo, getTodoList } from '../api/todo';
+import { Todo } from '../types/todoType';
+
+interface UseTodoHookProps {
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+export default function useTodoHook({ setTodos }: UseTodoHookProps) {
+  const [submitLoading, setSubmitLoading] = useState(false);
+  const [removeLoading, setRemoveLoading] = useState(false);
+
+  const addTodo = useCallback(
+    async (inputText: string) => {
+      try {
+        setSubmitLoading(true);
+        const title = inputText.trim();
+        if (!title) {
+          return alert('Please write something');
+        }
+        const newTodo = { title };
+        const { data } = await createTodo(newTodo);
+
+        setTodos(prev => [...prev, data]);
+      } catch (error) {
+        console.error(error);
+        alert('Something went wrong.');
+      } finally {
+        setSubmitLoading(false);
+      }
+    },
+    [setTodos],
+  );
+
+  const removeTodo = useCallback(
+    async (id: number) => {
+      try {
+        setRemoveLoading(true);
+        await deleteTodo(id);
+        setTodos(prev => prev.filter(item => item.id !== id));
+      } catch (error) {
+        console.error(error);
+        alert('Something went wrong.');
+      }
+    },
+    [setTodos],
+  );
+
+  return { addTodo, removeTodo, submitLoading, removeLoading };
+}
