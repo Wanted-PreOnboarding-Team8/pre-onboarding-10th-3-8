9fc210cfd7395470f7a39724a7e8b0889c235f74
Yena-Yun <cheryleduart@gmail.com>
2023-05-17 12:29:42 +0900

    Feat: 키워드 하이라이팅 추가

---
+++ src/utils/highlightedText.tsx
@@ -0,0 +1,21 @@
+export const highlightedText = (dropdownText: string, inputText: string) => {
+  if (inputText !== '' && dropdownText.includes(inputText)) {
+    const parts = dropdownText.split(new RegExp(`(${inputText})`, 'g'));
+
+    return (
+      <>
+        {parts.map((part, index) =>
+          part === inputText ? (
+            <span className="highlight" key={index}>
+              {part}
+            </span>
+          ) : (
+            part
+          ),
+        )}
+      </>
+    );
+  }
+
+  return dropdownText;
+};
