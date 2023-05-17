ea2db2b7ac4aeeb4f9f5027dd007cabbf0d119e6
kimyonghui <kyhui1115@gmail.com>
2023-05-17 15:05:52 +0900

    Env: eslintrc, package, tsconfig 설정추가 및 수정
    Co-authored-by: Daniel Daeyeon Kim <shaqok@users.noreply.github.com>
    Co-authored-by: 임예지 <1myeji@users.noreply.github.com>
    Co-authored-by: powercording <powercording@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,42 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    settings: {
-      react: {
-        version: 'detect',
+  },
+  extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'jsx-a11y'],
+  rules: {
+    'object-curly-newline': 0,
+    'arrow-parens': ['error', 'as-needed'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'linebreak-style': 0,
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      {
+        labelAttributes: ['htmlFor'],
       },
-    },
-    extends: [
-      'plugin:react/recommended',
-      'standard-with-typescript',
-      'plugin:prettier/recommended',
     ],
-    overrides: [],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      project: './tsconfig.json',
-    },
-    plugins: ['react', '@typescript-eslint'],
-    rules: {
-      'object-curly-newline': 0,
-      'arrow-parens': ['error', 'as-needed'],
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      'linebreak-style': 0,
-      'jsx-a11y/label-has-associated-control': [
-        2,
-        {
-          labelAttributes: ['htmlFor'],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    '@typescript-eslint/no-misused-promises': [
+      2,
+      {
+        checksVoidReturn: {
+          attributes: false,
         },
-      ],
-      'prettier/prettier': ['error', { endOfLine: 'auto' }],
-      '@typescript-eslint/no-misused-promises': [
-        2,
-        {
-          checksVoidReturn: {
-            attributes: false,
-          },
-        },
-      ],
-    },
-    ignorePatterns: ['node_modules/'],
-  };
+      },
+    ],
+  },
+  ignorePatterns: ['node_modules/'],
+};
