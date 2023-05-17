64ab6e30d5febfbbfd22f10a7a6954f7a20de19d
kimyonghui <kyhui1115@gmail.com>
2023-05-17 15:08:33 +0900

    Chore: 리액트 import
    Co-authored-by: powercording <powercording@users.noreply.github.com>
    Co-authored-by: 임예지 <1myeji@users.noreply.github.com>
    Co-authored-by: shaqok <shaqok@users.noreply.github.com>

--- src/utils/highlightedText.tsx
+++ src/utils/highlightedText.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const highlightedText = (dropdownText: string, inputText: string) => {
   if (inputText !== '' && dropdownText.includes(inputText)) {
     const parts = dropdownText.split(new RegExp(`(${inputText})`, 'g'));
