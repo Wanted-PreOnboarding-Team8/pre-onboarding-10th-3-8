05dba2d2d5f5a5e71af10e828ee7df3eeb547f39
kimyonghui <kyhui1115@gmail.com>
2023-05-17 16:50:48 +0900

    Refactor: CSS 클릭 active 코드 수정

--- src/components/DropdownItem.tsx
+++ src/components/DropdownItem.tsx
@@ -5,42 +5,36 @@ import { Todo } from 'types/todoType';
 
 interface DropdownItemProps {
   item: string;
-  idx: number;
   inputText: string;
   setInputText: React.Dispatch<React.SetStateAction<string>>;
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
   observer: ((node: any) => void) | null;
 }
 
-const DropdownItem = ({
-  item,
-  idx,
-  inputText,
-  setInputText,
-  setTodos,
-  observer,
-}: DropdownItemProps) => {
-  const [clickedItem, setClickedItem] = useState<number | null>(null);
+const DropdownItem = ({ item, inputText, setInputText, setTodos, observer }: DropdownItemProps) => {
+  const handleItemBackGround = (element: Element) => {
+    element.classList.add('clicked-item');
+    element.classList.remove('dropdown-item-hover');
+  };
 
-  const handleClick = async (item: string, index: number) => {
-    setClickedItem(index);
+  const handleClick = async (item: string, e: React.MouseEvent) => {
+    const element = e.target as Element;
+    handleItemBackGround(element);
 
     const dropdownNewItem = { title: item };
 
     const { data } = await createTodo(dropdownNewItem);
 
-    if (data) {
-      setTodos(prev => [...prev, data]);
-      setInputText('');
-    }
+    setTodos(prev => [...prev, data]);
+    setInputText('');
   };
 
   return (
     <li
       ref={observer}
-      className={`dropdown-item ${clickedItem === idx ? 'clicked-item' : 'dropdown-item-hover'}`}
-      onClick={() => {
-        handleClick(item, idx);
+      className="dropdown-item dropdown-item-hover"
+      onClick={e => {
+        handleClick(item, e);
       }}
     >
       {highlightedText(item, inputText)}
